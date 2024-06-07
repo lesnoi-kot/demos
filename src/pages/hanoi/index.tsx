1570cfcc09c926af8e3deb636b754cbed47c835f
yuko <yuriii.kovalyov@gmail.com>
2024-06-07 15:48:09 +0300

    mpa init

---
+++ src/pages/hanoi/index.tsx
@@ -0,0 +1,13 @@
+/* @refresh reload */
+import { render } from "solid-js/web";
+
+import "@/index.css";
+
+render(
+  () => (
+    <main>
+      <h1>Hello world!</h1>
+    </main>
+  ),
+  document.getElementById("root")!
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid()],
+  appType: "mpa",
+  plugins: [tsconfigPaths(), solid()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        hanoi: "./src/pages/hanoi/index.html",
+      },
+    },
+    emptyOutDir: true,
+  },
 });
