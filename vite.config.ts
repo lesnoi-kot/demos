44f750191d59c64fd56fcb8319ccb498edb93ca1
yuko <yuriii.kovalyov@gmail.com>
2024-06-13 21:48:37 +0300

    vite chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,40 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { visualizer } from "rollup-plugin-visualizer";
 import solid from "vite-plugin-solid";
 
+const root = "./src";
+const resolveRoot = (e: string) => path.resolve(__dirname, root, e);
+
 export default defineConfig({
+  root,
+  publicDir: path.resolve(__dirname, "public"),
   appType: "mpa",
-  plugins: [tsconfigPaths(), solid()],
+  plugins: [tsconfigPaths(), solid(), visualizer()],
   build: {
     rollupOptions: {
       input: {
-        main: "./index.html",
-        hanoi: "./src/pages/hanoi/index.html",
+        main: resolveRoot("index.html"),
+        hanoi: resolveRoot("pages/hanoi/index.html"),
+      },
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules/solid-js")) {
+            return "solid-js";
+          }
+
+          if (id.includes("node_modules/three")) {
+            return "three";
+          }
+
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
       },
     },
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
 });
